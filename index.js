5f92eb6ad0f448d9162aae847f1aa6c2498eb01a
SandeepAcumen <sandeepkumarg.acumenusa@gmail.com>
2025-02-21 12:15:19 +0530

    Index changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const odbc = require("odbc");
 const { Client } = require("pg");
 const app = express();
-const PORT = process.env.APP_PORT || 3600;
+const PORT = process.env.APP_PORT || 3000;
 
 const bodyParser = require("body-parser");
 const helmet = require("helmet");
@@ -135,7 +135,7 @@ app.post("/api/migrate", async (req, res) => {
     }
 });
 
-app.use(express.static('../microsoft-access-migrate/build'));
+app.use(express.static('../AccessFront/build'));
 
 
 app.get("/", (req, res) => {
